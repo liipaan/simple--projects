54413305ecf28a8701f01065a6c8ea04ceae6029
Moha Liban <maxamedliibaan394@gmail.com>
2026-05-28 11:58:02 +0300

    i should use only html,css and javaScript

---
+++ 1.calculator/main.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("result");
+
+function appendValue(value) {
+    result.value += value
+}
+
+function clearResult() {
+    result.value = ""
+}
+function deleteLast() {
+    result.value = result.value.slice(0, -1)
+
+}
+function Calculate() {
+    try {
+        result.value = eval(result.value)
+    } catch (error) {
+        result.value = "Error"
+    }
+}
